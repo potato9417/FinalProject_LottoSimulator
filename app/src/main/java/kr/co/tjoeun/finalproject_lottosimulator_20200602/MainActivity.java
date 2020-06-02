bfc32e5acc12ccecde0a4bbb75c33942da7a76b1
potato9417 <potato9417@gmail.com>
2020-06-02 11:36:14 +0900

    while로 반복구현 =>UI가 반영을 못함 => 응답없음(ANR) 으로 나옴

--- app/src/main/java/kr/co/tjoeun/finalproject_lottosimulator_20200602/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/finalproject_lottosimulator_20200602/MainActivity.java
@@ -51,6 +51,22 @@ public class MainActivity extends BaseActivity {
     @Override
     public void setupEvents() {
 
+        binding.buyAutoLottoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                while(true){
+                    makeLottoWinNumbers();
+                    checkWinRank();
+
+                    if(useMoney==1000000){
+                        break;
+                    }
+                }
+
+            }
+        });
+
         binding.buyOneLottoBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
