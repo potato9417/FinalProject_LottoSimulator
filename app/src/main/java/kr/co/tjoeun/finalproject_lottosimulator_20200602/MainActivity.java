65346c585254f7b4df466e2a8b2ea1a37afa123c
potato9417 <potato9417@gmail.com>
2020-06-02 12:18:59 +0900

    당첨번호 만드는 로직에대한 주석

--- app/src/main/java/kr/co/tjoeun/finalproject_lottosimulator_20200602/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/finalproject_lottosimulator_20200602/MainActivity.java
@@ -198,23 +198,30 @@ public class MainActivity extends BaseActivity {
     }
 
     void makeLottoWinNumbers(){
-
+//        지난주 당첨번호가 새 당첨번호에 영향 주는 것을 막기위한 조치
+//        기존 ㅏㄷㅇ첨번호를 모두 0으로 세팅
         for(int i=0;i<winLottoNumArr.length;i++){
 
             winLottoNumArr[i]=0;
 
         }
-
+//        보너스 번호도 0으로 세팅
         bonusNum=0;
 
+//        당처번호 6개를 뽑기위한 for문
         for(int i=0;i<winLottoNumArr.length;i++){
 
+//            조건에 맞는(중복이 아닌) 숫자를 뽑을때까지 무한반복
             while(true){
 
+//                1~45중 숫자를 랜덤
                 int randomNum = (int) (Math.random()*45+1);
 
+//                중복검사 결과를 저장하는 변수 => 일단 맞다고해놓고 틀리면 false로 변경
                 boolean isDuplicatedOk = true;
 
+//                당첨된 번호중 같은게 있다면 false로 변경
+//                한번도 같은게 없었다면 true 유지
                 for(int num:winLottoNumArr){
                     if(num==randomNum){
                         isDuplicatedOk=false;
@@ -222,18 +229,25 @@ public class MainActivity extends BaseActivity {
                     }
                 }
 
+//                중복검사가 통과되었다면
                 if(isDuplicatedOk){
+//                    당첨번호로 등록
                     winLottoNumArr[i] = randomNum;
+//                    무한반복 탈출 => 다음 당첨번호 뽑으러감 (for의 다음 i)
                     break;
                 }
             }
 
         }
 
+//        6개를 뽑는 for문이 다 돌고 나면 => 순서가 뒤죽박죽
+//        Arrays 클래스의 static 메쏘드를 사용해서 오름차순으로 정렬
         Arrays.sort(winLottoNumArr);
 
+//        보너스번호를 뽑는 무한 반복 => 숫자 하나만 뽑는다 => for문대신 while문사용
         while (true){
 
+//            1~45사이의 랜덤한 숫자(앞과같음)
             int randomNum = (int) (Math.random()*45+1);
 
             boolean isDuplicatedOk = true;
@@ -254,13 +268,16 @@ public class MainActivity extends BaseActivity {
 
         }
 
+//        당첨 번호들을 텍스트뷰에 표시
         for(int i=0;i<winNumTxts.size();i++){
 
             int winNum = winLottoNumArr[i];
 
+//            화면에 있는 당첨번호 텍스트뷰들을 ArrayList에 담아두고 (setValues참고) 활용
             winNumTxts.get(i).setText(winNum+"");
         }
 
+//        보너스번호도 화면에 표시
         binding.bonusNumTxt.setText(bonusNum+"");
 
     }
